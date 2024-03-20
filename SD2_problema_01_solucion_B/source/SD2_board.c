4718a02d3252cb3ca5449407c74447108d77d023
ValentinB <valentinbelliniz@gmail.com>
2024-03-20 18:34:45 -0300

    update board.h & board.c

--- SD2_problema_01_solucion_B/source/SD2_board.c
+++ SD2_problema_01_solucion_B/source/SD2_board.c
@@ -33,82 +33,123 @@
  *
  */
 
-/*==================[How to use]=============================================*/
-
-/*  Obtener valor SW1: key_getPressEv(BOARD_SW_ID_1)    */
-/*  Togglear led rojo: board_setLed(BOARD_LED_ID_ROJO, BOARD_LED_MSG_TOGGLE)    */
-/*  Encender led verde: board_setLed(BOARD_LED_ID_VERDE, BOARD_LED_MSG_ON)  */
-
-
-#ifndef SD2_BOARD_H_
-#define SD2_BOARD_H_
-
 /*==================[inclusions]=============================================*/
-#include "MKL43Z4.h"
-#include "stdbool.h"
+#include <SD2_board.h> /* SD2 board specific definitions */
+#include "fsl_port.h" /* for PORT_SetPinConfig() and other port functions */
+#include "fsl_gpio.h" /* for GPIO_PinInit() and other GPIO functions */
+#include "fsl_clock.h" /* for clock control */
+#include "pin_mux.h" /* for pin muxing */
 
-/*==================[cplusplus]==============================================*/
-#ifdef __cplusplus
-extern "C" {
-#endif
+/*==================[macros and definitions]=================================*/
 
-/*==================[macros]=================================================*/
-
-typedef enum
+/*==================[internal data declaration]==============================*/
+static const board_gpioInfo_type board_gpioLeds[] =
 {
-    BOARD_LED_ID_ROJO = 0,
-    BOARD_LED_ID_VERDE,
-    BOARD_LED_ID_TOTAL
-}board_ledId_enum;
+    {PORTE, GPIOE, 31},     /* LED ROJO */
+    {PORTD, GPIOD, 5},      /* LED VERDE */
+};
 
-typedef enum
+static const board_gpioInfo_type board_gpioSw[] =
 {
-    BOARD_SW_ID_1 = 0,
-    BOARD_SW_ID_3,
-    BOARD_SW_ID_TOTAL
-}board_swId_enum;
+    {PORTA, GPIOA, 4},      /* SW1 */
+    {PORTC, GPIOC, 3},      /* SW3 */
+};
 
-typedef enum
-{
-    BOARD_LED_MSG_OFF = 0,
-    BOARD_LED_MSG_ON,
-    BOARD_LED_MSG_TOGGLE
-}board_ledMsg_enum;
+/*==================[internal functions declaration]=========================*/
 
-typedef struct
-{
-    PORT_Type *port;
-    GPIO_Type *gpio;
-    uint32_t pin;
-}board_gpioInfo_type;
+/*==================[internal data definition]===============================*/
 
-/*==================[typedef]================================================*/
+/*==================[external data definition]===============================*/
 
-/*==================[external data declaration]==============================*/
+/*==================[internal functions definition]==========================*/
 
 /*==================[external functions definition]==========================*/
+void board_init(void)
+{
+	int32_t i;
+	gpio_pin_config_t gpio_led_config =
+	{
+		.outputLogic = 1,
+		.pinDirection = kGPIO_DigitalOutput, /* LED pins are configured as outputs */
+	};
+	gpio_pin_config_t gpio_sw_config = {
+		.pinDirection = kGPIO_DigitalInput, /* SW pins are configured as inputs */
+		.outputLogic = 0U
+	};
 
-/** \brief inicializacion del hardware
- **
- **/
-void board_init(void);
+	const port_pin_config_t port_led_config = {
+		/* Internal pull-up/down resistor is disabled */
+		.pullSelect = kPORT_PullDisable,
+		/* Slow slew rate is configured */
+		.slewRate = kPORT_SlowSlewRate,
+		/* Passive filter is disabled */
+		.passiveFilterEnable = kPORT_PassiveFilterDisable,
+		/* Low drive strength is configured */
+		.driveStrength = kPORT_LowDriveStrength,
+		/* Pin is configured as PTC3 */
+		.mux = kPORT_MuxAsGpio,
+	};
 
-/** \brief setea estado del led indicado
- **
- **/
-void board_setLed(board_ledId_enum id, board_ledMsg_enum msg);
+	const port_pin_config_t port_sw_config = {
+		/* Internal pull-up resistor is enabled */
+		.pullSelect = kPORT_PullUp,
+		/* Fast slew rate is configured */
+		.slewRate = kPORT_FastSlewRate,
+		/* Passive filter is disabled */
+		.passiveFilterEnable = kPORT_PassiveFilterDisable,
+		/* Low drive strength is configured */
+		.driveStrength = kPORT_LowDriveStrength,
+		/* Pin is configured as PTC3 */
+		.mux = kPORT_MuxAsGpio,
+	};
 
-/** \brief Devuelve estado del pulsador indicado
- **
- ** \return true: si el pulsdor esta apretado
- **         false: si el pulsador no esta apretado
- **/
-bool board_getSw(board_swId_enum id);
+	/* Enable clocks for ports used by the board */
+	CLOCK_EnableClock(kCLOCK_PortA);
+	CLOCK_EnableClock(kCLOCK_PortC);
+	CLOCK_EnableClock(kCLOCK_PortD);
+	CLOCK_EnableClock(kCLOCK_PortE);
 
-/*==================[cplusplus]==============================================*/
-#ifdef __cplusplus
+	/* C LEDs */
+	for (i = 0 ; i < BOARD_LED_ID_TOTAL ; i++)
+	{
+		PORT_SetPinConfig(board_gpioLeds[i].port, board_gpioLeds[i].pin, &port_led_config);
+		GPIO_PinInit(board_gpioLeds[i].gpio, board_gpioLeds[i].pin, &gpio_led_config);
+	}
+
+	PORTA->PCR[4] &= ~(7 << 8); //Desactivación de NMI en SW1
+
+	/* Initialize SWs */
+	for (i = 0 ; i < BOARD_SW_ID_TOTAL ; i++)
+	{
+		PORT_SetPinConfig(board_gpioSw[i].port, board_gpioSw[i].pin, &port_sw_config);
+		GPIO_PinInit(board_gpioSw[i].gpio, board_gpioSw[i].pin, &gpio_sw_config);
+	}
 }
-#endif
+
+void board_setLed(board_ledId_enum id, board_ledMsg_enum msg)
+{
+    switch (msg)
+    {
+        case BOARD_LED_MSG_OFF:
+        	GPIO_PortSet(board_gpioLeds[id].gpio, 1<<board_gpioLeds[id].pin);
+            break;
+
+        case BOARD_LED_MSG_ON:
+        	GPIO_PortClear(board_gpioLeds[id].gpio, 1<<board_gpioLeds[id].pin);
+            break;
+
+        case BOARD_LED_MSG_TOGGLE:
+        	GPIO_PortToggle(board_gpioLeds[id].gpio, 1<<board_gpioLeds[id].pin);
+            break;
+
+        default:
+            break;
+    }
+}
+
+bool board_getSw(board_swId_enum id)
+{
+    return !GPIO_PinRead(board_gpioSw[id].gpio, board_gpioSw[id].pin);
+}
 
 /*==================[end of file]============================================*/
-#endif /* SD2_BOARD_H_ */
