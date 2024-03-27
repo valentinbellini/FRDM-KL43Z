996c9b48f9217b2efbecac1bd0c1aa411af27fbb
ValentinB <valentinbelliniz@gmail.com>
2024-03-26 23:48:58 -0300

    fe de erratas: update key.h

--- SD2_Components/key.h
+++ SD2_Components/key.h
@@ -1,7 +1,6 @@
-/* Copyright 2017, DSI FCEIA UNR - Sistemas Digitales 2
+/* Copyright 2018, DSI FCEIA UNR - Sistemas Digitales 2
  *    DSI: http://www.dsi.fceia.unr.edu.ar/
- * Copyright 2017, Diego Alegrechi
- * Copyright 2017, Gustavo Muro
+ * Copyright 2018, Gustavo Muro
  * Copyright 2023, Guido Cicconi
  * All rights reserved.
  *
@@ -33,111 +32,35 @@
  *
  */
 
+#ifndef KEY_H_
+#define KEY_H_
+
 /*==================[inclusions]=============================================*/
-#include "key.h"
+#include "SD2_board.h"
 
-/*==================[macros and definitions]=================================*/
-typedef enum
-{
-	ESPERANDO_ACTIVACION = 0, // Estado esperando presionar pulsador.
-	ESPERANDO_DESACTIVACION	  // Estado esperando despresionar pulsador.
-}estPul_enum;
+/*==================[cplusplus]==============================================*/
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-/*==================[internal data declaration]==============================*/
+/*==================[macros]=================================================*/
 
-static estPul_enum estSW[BOARD_SW_ID_TOTAL]; // Arreglo que contiene el estado actual de cada tecla
-static int32_t timerSW[BOARD_SW_ID_TOTAL];   // Arreglo que contiene el tiempo transcurrido desde que se presionó cada tecla
-static bool pressSW[BOARD_SW_ID_TOTAL];      // Arreglo que indica si se ha detectado una pulsación de tecla
-static bool releaseSW[BOARD_SW_ID_TOTAL];    // Arreglo que indica si se ha detectado la liberación de una tecla
+/*==================[typedef]================================================*/
 
-/*==================[internal functions declaration]=========================*/
-
-/*==================[internal data definition]===============================*/
-
-/*==================[external data definition]===============================*/
-
-/*==================[internal functions definition]==========================*/
+/*==================[external data declaration]==============================*/
 
 /*==================[external functions definition]==========================*/
+extern void key_init(void);
+extern bool key_getPressEv(board_swId_enum id);
+extern bool key_getPressed(board_swId_enum id, int32_t timeMs);
+extern bool key_getReleaseEv(board_swId_enum id);
+extern void key_periodicTask1ms(void);
+extern void key_clearFlags(board_swId_enum id);
 
-
-extern void key_init(void) // Inicialización de los switches
-{
-    for (int32_t i = 0 ; i < BOARD_SW_ID_TOTAL ; i++)
-    {
-    	estSW[i] = ESPERANDO_ACTIVACION;  // Inicializa el estado de todas los switches como "esperando activación"
-    	ressSW[i] = 0;                    // Inicializa el indicador de pulsación de swtich como falso
-    	releaseSW[i] = 0;                 // Inicializa el indicador de liberación de switch como falso
-    	timerSW[i] = 0;                   // Inicializa el temporizador de cada switch en cero
-    }
+/*==================[cplusplus]==============================================*/
+#ifdef __cplusplus
 }
+#endif
 
-extern void key_periodicTask1ms(void) { // Tarea periódica de 1 ms para gestionar los switches
-
-	for (int32_t i = 0; i < BOARD_SW_ID_TOTAL; i++) { // Recorremos el array para todos los switches definidos en SD2_board.h
-
-		switch (estSW[i]) {
-
-		case ESPERANDO_ACTIVACION:
-			if (board_getSw(i)) {
-				pressSW[i] = 1;						// Se detecta una pulsada
-				estSW[i] = ESPERANDO_DESACTIVACION; // Cambiar estado a esperando despulsada
-			}
-			break;
-
-		case ESPERANDO_DESACTIVACION:
-			timerSW[i]++;							// Incrementa el temporizador mientras los tiene apretado
-			if (!board_getSw(i)) {
-				releaseSW[i] = 1;					// Se detecta liberación del switch
-				estSW[i] = ESPERANDO_ACTIVACION;	// Cambia el estado del switch
-				timerSW[i]=0;						// Setea timer/contador de dicho switch en cero nuevamente
-			}
-			break;
-
-		default:
-			estSW[i] = ESPERANDO_ACTIVACION; 		// Si se alcanza un estado desconocido, vuelve al estado de espera de activación
-			break;
-		}
-
-	}
-
-}
-
-extern bool key_getPressEv(board_swId_enum id) 	// Obtiene el evento de pulsación del switch
-{
-    bool ret = false;
-
-    if (pressSW[id])
-    {
-        pressSW[id] = 0;	// Reinicia el indicador de pulsado del switch
-        ret = true;			// Devuelve verdadero indicando que se ha detectado una pulsación de switch
-    }
-
-    return ret;
-}
-
-extern bool key_getReleaseEv(board_swId_enum id) // Obtiene el evento de liberación del switch
-{
-    bool ret = false;
-
-    if (releaseSW[id])
-    {
-    	releaseSW[id] = 0;  // Reinicia el indicador de liberación del switch
-        ret = true;			// Devuelve verdadero indicando que se ha detectado una liberación del switch
-    }
-
-    return ret;
-}
-
-// Comprueba si el switch ha estado presionado durante un tiempo específico:
-extern bool key_getPressed(board_swId_enum id, int32_t timeMs){
-	if (timerSW[id] >= timeMs) return true; // Si el tiempo transcurrido es mayor o igual al tiempo especificado, devuelve verdadero
-	else return false;
-}
-
-// Limpia los indicadores de eventos de un switch específico:
-extern void key_clearFlags(board_swId_enum id)
-{
-	pressSW[id] = 0;    // Reinicia el indicador de pulsación del switch
-	releaseSW[id] = 0;  // Reinicia el indicador de liberación del switch
-}
+/*==================[end of file]============================================*/
+#endif /* KEY_H_ */
