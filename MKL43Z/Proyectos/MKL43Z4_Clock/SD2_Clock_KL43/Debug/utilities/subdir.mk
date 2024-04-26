3a0f21a1ad5fa7fc979974a8715720a9c2b1eb2b
ValentinB <valentinbelliniz@gmail.com>
2024-04-26 11:43:22 -0300

    MKL43Z_SD2_I2C_ACCEL file

---
+++ MKL43Z/Proyectos/MKL43Z4_Clock/SD2_Clock_KL43/Debug/utilities/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../utilities/fsl_debug_console.c \
+../utilities/fsl_str.c 
+
+OBJS += \
+./utilities/fsl_debug_console.o \
+./utilities/fsl_str.o 
+
+C_DEPS += \
+./utilities/fsl_debug_console.d \
+./utilities/fsl_str.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+utilities/%.o: ../utilities/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU C Compiler'
+	arm-none-eabi-gcc -D__REDLIB__ -DCPU_MKL43Z256VLH4 -DCPU_MKL43Z256VLH4_cm0plus -DFSL_RTOS_BM -DSDK_OS_BAREMETAL -DSDK_DEBUGCONSOLE=0 -DCR_INTEGER_PRINTF -DPRINTF_FLOAT_ENABLE=0 -DSERIAL_PORT_TYPE_UART=1 -D__MCUXPRESSO -D__USE_CMSIS -DDEBUG -I"C:\Users\guido\Documents\MCUXpressoIDE_11.3.0_5222\workspace\Docencia SD2\SD2_Clock_KL43\board" -I"C:\Users\guido\Documents\MCUXpressoIDE_11.3.0_5222\workspace\Docencia SD2\SD2_Clock_KL43\source" -I"C:\Users\guido\Documents\MCUXpressoIDE_11.3.0_5222\workspace\Docencia SD2\SD2_Clock_KL43" -I"C:\Users\guido\Documents\MCUXpressoIDE_11.3.0_5222\workspace\Docencia SD2\SD2_Clock_KL43\drivers" -I"C:\Users\guido\Documents\MCUXpressoIDE_11.3.0_5222\workspace\Docencia SD2\SD2_Clock_KL43\component\uart" -I"C:\Users\guido\Documents\MCUXpressoIDE_11.3.0_5222\workspace\Docencia SD2\SD2_Clock_KL43\component\serial_manager" -I"C:\Users\guido\Documents\MCUXpressoIDE_11.3.0_5222\workspace\Docencia SD2\SD2_Clock_KL43\utilities" -I"C:\Users\guido\Documents\MCUXpressoIDE_11.3.0_5222\workspace\Docencia SD2\SD2_Clock_KL43\component\lists" -I"C:\Users\guido\Documents\MCUXpressoIDE_11.3.0_5222\workspace\Docencia SD2\SD2_Clock_KL43\device" -I"C:\Users\guido\Documents\MCUXpressoIDE_11.3.0_5222\workspace\Docencia SD2\SD2_Clock_KL43\CMSIS" -O0 -fno-common -g3 -Wall -c -ffunction-sections -fdata-sections -ffreestanding -fno-builtin -fmerge-constants -fmacro-prefix-map="../$(@D)/"=. -mcpu=cortex-m0plus -mthumb -D__REDLIB__ -fstack-usage -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ MKL43Z/README.md
@@ -0,0 +1,10 @@
+# Introducción
+Para los proyectos en este repositorio se utilizarán las placas de desarrollo de NXP FRDM-KL43Z y FRDM-KL46Z, ambas cuenta con arquitectura ARMv6M y procesador Cortex M0+.
+
+## Contenido
+
+- [SD2_board_KL43](/SD2_board_KL43/SD2_KL43Z_board.c): Contiene código con definiciones y funciones para el manejo de LEDS y Switches para la placa de desarrollo FRDM-KL43Z4. Tambien se definen algunas macros y estructuras de datos internas que describen los pines utilizados.
+
+- [SD2_Components](/SD2_Components/key.c): contiene algunos componentes reutilizables para diferentes proyectos tales como `key.c` que se utiliza para manejar el pulsado de los switches.
+
+- [Proyectos](/MKL43Z/Proyectos/README.md): Contiene una lista de proyectos realizados para dicha placa de desarrollo. Algunos son soluciones, otros son simplemente codigo realizado para ver algun tema de registros o manejo de instrucciones en particular. 
